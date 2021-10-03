eefecfb2d1273fe47b61d10a7833d7db239db128
LosSantosSavior <nickrussell001@gmail.com>
2021-10-03 14:12:58 -0400

    proj2

--- src/Order.java
+++ src/Order.java
@@ -1,2 +1,16 @@
 public class Order {
+    private ShippingAddress destination;
+    private Customer orderedBy;
+
+    public String getDestination()  {
+
+    }
+
+    public String getOrderer()  {
+
+    }
+
+    public Order(ShippingAddress dest, Customer cust)  {
+
+    }
 }
