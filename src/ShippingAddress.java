845f3de52f842e18b15b51dafd03b85d6d2190f9
LosSantosSavior <nickrussell001@gmail.com>
2021-10-03 14:11:37 -0400

    proj2

--- src/ShippingAddress.java
+++ src/ShippingAddress.java
@@ -1,2 +1,16 @@
 public class ShippingAddress {
+    private String AddressLine1;
+    private String AddressLine2;
+    private String City;
+    private String State;
+    private String PostalCode;
+
+    public String toString()    {
+
+    }
+
+    public ShippingAddress(String line1, String line2, String cityName, String stateName, String zipCode)    {
+
+    }
+
 }
