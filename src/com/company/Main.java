1d8dbc9d2b23d57ebfbc0ade5c7f85189b68f394
LosSantosSavior <nickrussell001@gmail.com>
2021-10-03 14:08:32 -0400

    proj2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,7 +18,7 @@ public class Main {
     public Customer selectCustomer() {
 
     }
-    public manageCustomer(Customer selectedCustomer)    {
+    public manageCustomer(Customer selectCustomer)    {
 
     }
 
