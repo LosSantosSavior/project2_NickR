26024c263ac47f6a7a6d99135f485f95b4fc9169
LosSantosSavior <nickrussell001@gmail.com>
2021-10-03 20:18:40 -0400

    proj2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,18 @@
 package com.company;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 
 public class Main {
 
     private ArrayList<Order> Orders;
     private ArrayList<Customer> Customers;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        var fileName = "store.txt";
+        var allLines = Files.readString(Paths.get(fileName));
 
     }
     public void Store() {
