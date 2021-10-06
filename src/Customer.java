f2df2d2bb43144668ac14325fed4542bc6b42af9
LosSantosSavior <nickrussell001@gmail.com>
2021-10-06 19:18:44 -0400

    proj2

--- src/Customer.java
+++ src/Customer.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Customer {
     private ArrayList<ShippingAddress> Addresses;
@@ -6,18 +7,18 @@ public class Customer {
     private int customerID;
 
     public String getName() {
-
+        return Name;
     }
 
     public ShippingAddress addAddress(ShippingAddress newAddress)  {
-
+        System.out.println("Enter your address:");
     }
 
     public Customer customerName(String custName)  {
-
+        System.out.println("");
     }
 
     public String toString()    {
-
+        return "Address: " + Addresses + "\nName: " + Name + "Customer ID: " + customerID;
     }
 }
